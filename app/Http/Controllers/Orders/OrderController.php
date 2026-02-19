72d442b8fbb1831209950e45410756ae469f79d1
I_s_L_a_m <akis28dze@gmail.com>
2026-02-19 16:40:41 +0000

    feat: add index and show to orders (#1)

--- app/Http/Controllers/Orders/OrderController.php
+++ app/Http/Controllers/Orders/OrderController.php
@@ -8,4 +8,14 @@ use Illuminate\Http\Request;
 class OrderController extends Controller
 {
     //
+    public function index()
+    {
+        return response()->json(['message' => 'Order Index']);
+    }
+
+
+    public function show($id)
+    {
+        return response()->json(['message' => 'Order Details for ID:' . $id]);
+    }
 }
