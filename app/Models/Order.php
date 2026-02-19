7f9b3fdd8e68eccc6327100a42668279277bb812
akis-dz <akis28dze@gmail.com>
2026-02-19 14:43:18 +0000

    feat: add columns to orders table

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -7,4 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     //
+    protected $fillable = [
+        'id',
+        'order_number',
+        'customer_name',
+        'phone',
+
+    ];
+
+    public function items(){
+        return $this->belongsTo(Item::class, 'order_id');
+    }
 }
