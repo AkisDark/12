dcd4f2104754c5b0f1e51a7ddb6e2fe013f421aa
AkisDark <akis.akka.islam@gmail.com>
2026-02-19 15:46:49 +0100

    feat: add column to items table

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -7,4 +7,21 @@ use Illuminate\Database\Eloquent\Model;
 class Item extends Model
 {
     //
+
+    protected $fillable = [
+        'order_id',
+        'product_id',
+        'quantity',
+        'price',
+    ];
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
